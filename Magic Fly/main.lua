4f0558756ce849a1360822940897003c0437ef84
Alejandro Samper <alejandro.samper@insbaixcamp.cat>
2013-06-07 19:31:53 +0200

    New main

--- Magic Fly/main.lua
+++ Magic Fly/main.lua
@@ -1,17 +1,15 @@
 --
--- Project: Test
--- Description: 
+-- Project: Magic Fly School by Nyam Systems
+-- Description: Designed and Produced by Nyam Systems®
 --
 -- Version: 1.0
 -- Managed with http://CoronaProjectManager.com
 --
 -- Copyright 2013 . All Rights Reserved.
 -- 
-
 local storyboard = require("storyboard")
 display.setStatusBar(display.HiddenStatusBar)
-print("Wola")
+--print("Nyam System presents... 		-=Magic Fly School=-")
 centerX = display.contentWidth * .5
 centerY = display.contentHeight * .5 
-storyboard.gotoScene ( "start", { effect = "fade" } )
-
+storyboard.gotoScene ( "main_start", { effect = "fade" } )
